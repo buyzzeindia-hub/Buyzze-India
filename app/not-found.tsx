204a2bb5140b446936cd448609915fbd53c4f9fe
Shubhcoder123 <shubhampandit0809@gmail.com>
2026-06-20 13:53:45 +0530

    not found issue

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const dynamic = 'force-dynamic';
+
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
